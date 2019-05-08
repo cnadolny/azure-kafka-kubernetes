c5d164adb73362567c562b9d136b28488109f880
Claudia Nadolny <nadolny.claudia@gmail.com>
2019-05-08 00:09:22 -0700

    updated to use helm chart

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# kafka-kubernetes
-
-Modified script from [kow3ns/kubernetes/kafka](https://github.com/kow3ns/kubernetes-kafka/).
-
 ## Run
 
 This script assumes you already have the Azure CLI and kubectl already installed.
@@ -9,24 +5,16 @@ This script assumes you already have the Azure CLI and kubectl already installed
 If you wish to provision a cluster, uncomment below lines in ```install.sh```:
 
 ```bash
-# echo "Creating resource group"
-# echo ". name:  $RG_NAME"
-# az group create -n $RG_NAME -l $LOCATION -o tsv >> log.txt
-
-# echo "Creating AKS cluster"
-# echo ".name: $CLUSTER_NAME"
-# echo ". location: $LOCATION"
-# az aks create -n $CLUSTER_NAME -g $RG_NAME -l $LOCATION --generate-ssh-keys -o tsv >> log.txt
-
-# echo "Setting $CLUSTER_NAME as current context"
-# az aks get-credentials -n $CLUSTER_NAME -g $RG_NAME -o tsv >> log.txt
+az group create -n $RG_NAME -l $LOCATION
+az aks create -n $CLUSTER_NAME -g $RG_NAME -l $LOCATION --generate-ssh-keys
+az aks get-credentials -n $CLUSTER_NAME -g $RG_NAME --overwrite-existing
 ```
 
 If you already have Kubernetes cluster deployed, change the ```CLUSTER_NAME```, ```RG_NAME``` and ```LOCATION``` in ```install.sh``` to corresponding values from your deployment.
 
 ```bash
-export CLUSTER_NAME="kafka-k8-cluster1"
-export RG_NAME="kafka-k81"
+export CLUSTER_NAME="kafka-k8-cluster"
+export RG_NAME="kafka-k8"
 export LOCATION="westus2"
 ```
 
@@ -34,7 +22,7 @@ export LOCATION="westus2"
 
 Run ```./install.sh```
 
-This script will install a Kafka instance with two brokers and two Zookeeper instances on your AKS cluster, with external access via a loadbalancer. Currently it only supports PLAINTEXT authentication.
+This script will install a Kafka instance with three brokers and three Zookeeper instances on your AKS cluster, with external access via a loadbalancer. Currently it only supports PLAINTEXT authentication.
 
 ## Test
 
@@ -49,13 +37,13 @@ kubectl get svc
 To list the topics on this Kafka cluster:
 
 ```bash
-kubectl -n kafka exec testclient -- ./bin/kafka-topics.sh --zookeeper <publicIP>:2181 --list
+kubectl exec kafkaclient -- ./bin/kafka-topics.sh --zookeeper kafka-zookeeper:2181 --list
 ```
 
 To create a topic ```test``` on the Kafka cluster with 4 partitions:
 
 ```bash
-kubectl -n kafka exec testclient -- ./bin/kafka-topics.sh --create --zookeeper zk-cs:2181 --replication-factor 1 --partitions 4 --topic test
+kubectl exec kafkaclient -- ./bin/kafka-topics.sh --create --zookeeper kafka-zookeeper:2181 --replication-factor 1 --partitions 4 --topic test
 ```
 
 ### Using Kafkacat
@@ -80,7 +68,7 @@ Enter below command. Just type (or paste) your message into the terminal and hit
 Once you’re done just hit control+C
 
 ```bash
-kafkacat -b <publicIP>:9092 -t new_topic -P
+kafkacat -b </publicIP>/:31090 -t new_topic -P
 ```
 
 **To create consumers from command-line:**
@@ -88,5 +76,5 @@ kafkacat -b <publicIP>:9092 -t new_topic -P
 Enter below command. Once you’re done just hit control+C
 
 ```bash
-kafkacat -b <publicIP>:9092 -t new_topic -C
+kafkacat -b </publicIP>/:31090 -t new_topic -C
 ```
--- install.sh
+++ install.sh
@@ -1,70 +1,80 @@
 #!/bin/bash
 
 echo "Running script to create Kafka on Kubernetes cluster"
-echo "Running script to create Kafka on Kubernetes cluster"
 
-export CLUSTER_NAME="kafka-k8-cluster1"
-export RG_NAME="kafka-k81"
+# Set correct values for your subscription
+export CLUSTER_NAME="kafka-k8-cluster"
+export RG_NAME="kafka-k8"
 export LOCATION="westus2"
 
-# # Uncomment these lines if you would like to create an AKS cluster
-# echo "Creating resource group"
-# echo ". name:  $RG_NAME"
-# az group create -n $RG_NAME -l $LOCATION -o tsv >> log.txt
+export KAFKA_IP_NAME_0="kafka-ip-0"
+export KAFKA_IP_NAME_1="kafka-ip-1"
+export KAFKA_IP_NAME_2="kafka-ip-2"
 
-# echo "Creating AKS cluster"
-# echo ".name: $CLUSTER_NAME"
-# echo ". location: $LOCATION"
-# az aks create -n $CLUSTER_NAME -g $RG_NAME -l $LOCATION --generate-ssh-keys -o tsv >> log.txt
+echo "Creating AKS Cluster."
 
-# echo "Setting $CLUSTER_NAME as current context"
-# az aks get-credentials -n $CLUSTER_NAME -g $RG_NAME -o tsv >> log.txt
+# az group create -n $RG_NAME -l $LOCATION
+# az aks create -n $CLUSTER_NAME -g $RG_NAME -l $LOCATION --generate-ssh-keys
+# az aks get-credentials -n $CLUSTER_NAME -g $RG_NAME --overwrite-existing
+
+echo "Creating public IPs."
 
 export CLUSTER_RG="$(az aks show -g $RG_NAME -n $CLUSTER_NAME --query nodeResourceGroup -o tsv)"
 
-export KAFKA_IP_NAME_0="kafka_ip_0"
-export KAFKA_IP_NAME_1="kafka_ip_1"
+az network public-ip create -g $CLUSTER_RG -n $KAFKA_IP_NAME_0 --allocation-method static
+az network public-ip create -g $CLUSTER_RG -n $KAFKA_IP_NAME_1 --allocation-method static
+az network public-ip create -g $CLUSTER_RG -n $KAFKA_IP_NAME_2 --allocation-method static
 
-echo "Creating public ip"
-echo ". name: $KAFKA_IP_NAME_0"
-az network public-ip create -g $CLUSTER_RG -n $KAFKA_IP_NAME_0 --allocation-method static -o tsv >> log.txt
+KAFKA_IP_0="$(az network public-ip show --resource-group $CLUSTER_RG --name $KAFKA_IP_NAME_0 --query ipAddress)"
+KAFKA_IP_1="$(az network public-ip show --resource-group $CLUSTER_RG --name $KAFKA_IP_NAME_1 --query ipAddress)"
+KAFKA_IP_2="$(az network public-ip show --resource-group $CLUSTER_RG --name $KAFKA_IP_NAME_2 --query ipAddress)"
 
-KAFKA_IP_0=""
-while [ -z $KAFKA_IP_0 ]; do
-  KAFKA_IP_0="$(az network public-ip show --resource-group $CLUSTER_RG --name $KAFKA_IP_NAME_0 --query ipAddress --output tsv)"
-  [ -z "$KAFKA_IP_0" ] && sleep 10
-  echo "Waiting on external IP..."
+echo -e "adding RBAC ServiceAccount and ClusterRoleBinding for tiller\n\n"
+export timeout=120  #Number of loops before timeout on check on tiller
+export wait=5 #Number of seconds between to checks on tiller
+
+kubectl create serviceaccount --namespace kube-system tillersa
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Creation of tillersa failed"
+    # exit 1
+fi
+
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tillersa
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Creation of the tiller-cluster-rule failed"
+    # exit 1
+fi
+
+kubectl create clusterrolebinding dashboard-admin --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Creation of dashboard-admin failed"
+    # exit 1
+fi
+
+echo "Upgrading tiller (helm server) to match client version."
+
+helm init --upgrade --service-account tillersa
+if [ $? -ne 0 ]; then
+    echo "[ERROR] The helm init command failed"
+    # exit 1
+fi
+
+count=0
+until kubectl get pods --all-namespaces | grep -E "kube-system(\s){3}tiller.*1\/1\s*Running+"
+do
+        sleep ${wait}
 done
-echo "IP created: $KAFKA_IP_0"
 
-echo "Creating public ip"
-echo ". name: $KAFKA_IP_NAME_1"
-az network public-ip create -g $CLUSTER_RG -n $KAFKA_IP_NAME_1 --allocation-method static >> log.txt
+echo "tiller upgrade complete."
 
-KAFKA_IP_1=""
-while [ -z $KAFKA_IP_1 ]; do
-  KAFKA_IP_1="$(az network public-ip show --resource-group $CLUSTER_RG --name $KAFKA_IP_NAME_1 --query ipAddress --output tsv)"
-  [ -z "$KAFKA_IP_1" ] && sleep 10
-  echo "Waiting on external IP..."
-done
-echo "IP created: $KAFKA_IP_1"
+echo "adding kafkaClient.yaml"
 
-echo "Creating Namespace kafka"
-kubectl create namespace kafka
+kubectl create -f kafkaClient.yaml
 
-echo "Running zookeeper.yaml"
-kubectl create -f zookeeper.yaml
+echo "installing kafka helm chart"
 
-echo "Running kafka.yaml"
-cat kafka.yaml | \
-sed 's/\$KAFKA_IP_0'"/$KAFKA_IP_0/g" | \
-sed 's/\$KAFKA_IP_1'"/$KAFKA_IP_1/g" | \
-kubectl apply -f -
-
-for i in {0..1}; do kubectl label pods kafka-$i pod-name=kafka-$i;done
-
-echo "Running testclient.yaml"
-kubectl create -f testclient.yaml
-
-echo "Running kubectl get all"
-kubectl get all 
+cat values.yaml | \
+sed 's/\${KAFKA_IP_0}'"/$KAFKA_IP_0/g" | \
+sed 's/\${KAFKA_IP_1}'"/$KAFKA_IP_1/g" | \
+sed 's/\${KAFKA_IP_2}'"/$KAFKA_IP_2/g" | \
+helm install --name kafka incubator/kafka -f -
