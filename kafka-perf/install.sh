159b726f2fcfb5551de5b2e01a55186d4660b1c5
Rami Sayar <rami.sayar@microsoft.com>
2019-07-01 11:57:17 -0400

    Refactored aks install script.

--- kafka-perf/install.sh
+++ kafka-perf/install.sh
@@ -1,67 +1,14 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 echo "Running script to create Kafka on Kubernetes cluster"
 
-# Set correct values for your subscription
-if [ -z "$CLUSTER_NAME" ]; then
-      CLUSTER_NAME="kafka-perf"
-fi
-if [ -z "$RG_NAME" ]; then
-      RG_NAME="kafka-perf"
-fi
-if [ -z "$LOCATION" ]; then
-      RG_NAME="westus2"
-fi
-if [ -z "$VM_SIZE" ]; then
-      VM_SIZE="Standard_DS4_v2"
-fi
-if [ -z "$NODE_COUNT" ]; then
-      NODE_COUNT=7
-fi
+export CLUSTER_NAME="kafka-perf"
+export RG_NAME="kafka-perf"
+export LOCATION="westus2"
+export NODE_SIZE="Standard_DS4_v2"
+export NODE_COUNT="7"
 
-echo "Creating AKS Cluster"
-
-az group create -n $RG_NAME -l $LOCATION
-az aks create -n $CLUSTER_NAME -g $RG_NAME -l $LOCATION --node-count $NODE_COUNT --node-vm-size $VM_SIZE --generate-ssh-keys 
-az aks get-credentials -n $CLUSTER_NAME -g $RG_NAME --overwrite-existing
-
-echo -e "adding RBAC ServiceAccount and ClusterRoleBinding for tiller\n\n"
-export timeout=120  #Number of loops before timeout on check on tiller
-export wait=5 #Number of seconds between to checks on tiller
-
-kubectl create serviceaccount --namespace kube-system tillersa
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Creation of tillersa failed"
-    # exit 1
-fi
-
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tillersa
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Creation of the tiller-cluster-rule failed"
-    # exit 1
-fi
-
-kubectl create clusterrolebinding dashboard-admin --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Creation of dashboard-admin failed"
-    # exit 1
-fi
-
-echo "Upgrading tiller (helm server) to match client version."
-
-helm init --upgrade --service-account tillersa
-if [ $? -ne 0 ]; then
-    echo "[ERROR] The helm init command failed"
-    # exit 1
-fi
-
-count=0
-until kubectl get pods --all-namespaces | grep -E "kube-system(\s){3}tiller.*1\/1\s*Running+"
-do
-        sleep ${wait}
-done
-
-echo "tiller upgrade complete."
+. ../utils/aks_setup.sh
 
 echo "installing kafka helm chart"
 
