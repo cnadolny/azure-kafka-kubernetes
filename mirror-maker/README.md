28462fb258c7afb820a48db30e131200e15ff208
Claudia Nadolny <nadolny.claudia@gmail.com>
2019-07-09 08:57:39 -0700

    working changes

--- mirror-maker/README.md
+++ mirror-maker/README.md
@@ -1,16 +1,5 @@
-# mirror-maker-kubernetes
+# Replicating Data through Kafka Mirror Maker on Kubernetes
 
-To run this project, change the following values in the kafka-mm.yaml file:
+Internally, through peered VNETS
 
-- CONSUMER\_IP
-- PRODUCER\_ENDPOINT
-- PRODUCER\_PASSWORD
-
-Where it is assumed the CONSUMER\_IP is your Kafka
-on Kubernetes endpoint, and the PRODUCER\_ENDPOINT and
-PRODUCER\_PASSWORD are your Kafka on
-Event Hub Credentials.
-
-Then, apply the deployment:
-
-kubectl -f kafka-mm.yaml
+Externally, through a public endpoint
--- mirror-maker/install.sh
+++ mirror-maker/install.sh
@@ -1,25 +1,34 @@
+#! /usr/bin/env bash
+
 export LOCATION1=westus2
 export LOCATION2=southcentralus
-export RG_NAME1=mirror-maker-$LOCATION1
+export RG1=mirror-maker-$LOCATION1
 export AKS_NAME1=mirror-maker-$LOCATION1
-export RG_NAME2=mirror-maker-$LOCATION2
+export RG2=mirror-maker-$LOCATION2
 export AKS_NAME2=mirror-maker-$LOCATION2
 export VNET1=mirror-maker-vnet-$LOCATION1
 export VNET2=mirror-maker-vnet-$LOCATION2
+export VNET1_PEERING=mm-$LOCATION1-$LOCATION2
+export VNET2_PEERING=mm-$LOCATION2-$LOCATION1
 export SUBNET_NAME1=aks-subnet-$LOCATION1
 export SUBNET_NAME2=aks-subnet-$LOCATION2
 
-az network vnet create -g $RG1 -n $VNET1 --address-prefix 10.1.0.0/16 --subnet-name aks-subnet1 --subnet-prefix 10.1.0.0/24
-az network vnet create -g $RG2 -n $VNET2 --address-prefix 10.2.0.0/16 --subnet-name aks-subnet2 --subnet-prefix 10.2.0.0/24
-
-export VNET1_SUBNET_ID = $(az network vnet subnet show -g $RG1 -n $SUBNET_NAME1 --vnet-name $VNET1 --query id -o tsv)
-export VNET2_SUBNET_ID = $(az network vnet subnet show -g $RG2 -n $SUBNET_NAME2 --vnet-name $VNET2 --query id -o tsv)
-
 az group create -n $RG1 -l $LOCATION1
-az aks create -n $AKS_NAME1 -l $LOCATION1 --generate-ssh-keys --vnet-subnet-id $VNET1_SUBNET_ID
-
 az group create -n $RG2 -l $LOCATION2
-az aks create -n $AKS_NAME2 -l $LOCATION2 --generate-ssh-keys --vnet-subnet-id $VNET2_SUBNET_ID
+az network vnet create -g $RG1 -n $VNET1 --address-prefix 10.1.0.0/16 --subnet-name $SUBNET_NAME1 --subnet-prefix 10.1.0.0/24
+az network vnet create -g $RG2 -n $VNET2 --address-prefix 10.2.0.0/16 --subnet-name $SUBNET_NAME2 --subnet-prefix 10.2.0.0/24
+
+export VNET1_ID=$(az network vnet show -g $RG1 -n $VNET1 --query id --out tsv)
+export VNET2_ID=$(az network vnet show -g $RG2 -n $VNET2 --query id --out tsv)
+
+az network vnet peering create -n $VNET1_PEERING -g $RG1 --vnet-name $VNET1 --remote-vnet $VNET2_ID --allow-vnet-access
+az network vnet peering create -n $VNET2_PEERING -g $RG2 --vnet-name $VNET2 --remote-vnet $VNET1_ID --allow-vnet-access
+
+export VNET1_SUBNET_ID=$(az network vnet subnet show -g $RG1 -n $SUBNET_NAME1 --vnet-name $VNET1 --query id -o tsv)
+export VNET2_SUBNET_ID=$(az network vnet subnet show -g $RG2 -n $SUBNET_NAME2 --vnet-name $VNET2 --query id -o tsv)
+
+az aks create -g $RG1 -n $AKS_NAME1 -l $LOCATION1 --generate-ssh-keys --vnet-subnet-id $VNET1_SUBNET_ID --kubernetes-version 1.14.0 --load-balancer-sku standard --enable-vmss
+az aks create -g $RG2 -n $AKS_NAME2 -l $LOCATION2 --generate-ssh-keys --vnet-subnet-id $VNET2_SUBNET_ID --kubernetes-version 1.14.0 --load-balancer-sku standard --enable-vmss
 
 az aks get-credentials -n $AKS_NAME1 -g $RG1 --overwrite-existing
 
@@ -37,19 +46,8 @@ echo "Installing Kafka"
 
 kubectl create -n kafka -f ../kafka-operator-strimzi/simple-kafka.yaml
 
-kubectl create -f `
-apiVersion: v1
-kind: Service
-metadata:
-  name: zk-internal-app
-  annotations:
-    service.beta.kubernetes.io/azure-load-balancer-internal: "true"
-spec:
-  type: LoadBalancer
-  ports:
-  - port: 2181
-  selector:
-    app: cp-zookeeper`
+echo "Creating internal load balancer"
+kubectl create -f internal-lb.yaml
 
 az aks get-credentials -n $AKS_NAME2 -g $RG2 --overwrite-existing
 
@@ -66,3 +64,6 @@ helm install strimzi/strimzi-kafka-operator --namespace kafka --name kafka-opera
 echo "Installing Kafka"
 
 kubectl create -n kafka -f ../kafka-operator-strimzi/simple-kafka.yaml
+
+echo "Creating internal load balancer"
+kubectl create -f internal-lb.yaml
